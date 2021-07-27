a402f330f70a59d4e6817f642950bcbdbc7b9d16
surut555 <63528254+surut555@users.noreply.github.com>
2021-07-28 00:28:37 +0700

    update api

--- api/connected.php
+++ api/connected.php
@@ -3,4 +3,4 @@
     error_reporting(0);
     error_reporting(E_ERROR | E_PARSE);
     header("content-type:text/javascript;charset=utf-8");
-    $link = mysqli_connect('localhost', 'Setthahana', 'surut4624430', "shoppimgmall");
+    $link = mysqli_connect('localhost', 'surut', 'Surut4624430', "shoppingmall");
